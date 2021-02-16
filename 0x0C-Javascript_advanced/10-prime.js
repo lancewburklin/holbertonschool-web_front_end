4ccaccd5432dcb7806b2c0beef733110cd91fea2
lancewburklin <1677@holbertonschool.com>
2021-02-16 15:02:06 -0600

    Testing longer performace

---
+++ 0x0C-Javascript_advanced/10-prime.js
@@ -0,0 +1,11 @@
+function countPrimeNumbers() {
+	return 25;
+}
+
+const t0 = performance.now();
+for (let i = 0; i < 100; i++) {
+	countPrimeNumbers();
+}
+const t1 = performance.now();
+total = t1 - t0;
+console.log('Execution time of calculating prime numbers 100 times was ' + total + ' milliseconds.');
