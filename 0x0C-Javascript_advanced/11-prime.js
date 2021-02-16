1376fb1e6ce4d8718ff578c06c8f451116dc12bc
lancewburklin <1677@holbertonschool.com>
2021-02-16 15:52:34 -0600

    Counting primes with a timer

---
+++ 0x0C-Javascript_advanced/11-prime.js
@@ -0,0 +1,28 @@
+function countPrimeNumbers() {
+	let count = 0;
+	function test_prime(n) {
+		if (n === 2) {
+			return true;
+		}
+		for (let i = 2; i < n; i++) {
+			if (n % i === 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+	for (let num = 2; num < 101; num++) {
+		if (test_prime(num)) {
+			count += 1;
+		}
+	}
+	return count;
+}
+
+const t0 = performance.now();
+for (let i = 0; i < 100; i++) {
+	setTimeout(countPrimeNumbers(), 0);
+}
+const t1 = performance.now();
+total = t1 - t0;
+console.log('Execution time of calculating prime numbers 100 times was ' + total + ' milliseconds.');
